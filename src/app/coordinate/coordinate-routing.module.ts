a6900fcb43aae99728d006a8f3fd41132c8d032b
Gerard <gerardmonso@gmail.com>
2023-11-21 19:06:50 +0100

    coordinates routing components

--- src/app/coordinate/coordinate-routing.module.ts
+++ src/app/coordinate/coordinate-routing.module.ts
@@ -1,12 +1,20 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {CoordinateListComponent} from "./coordinate-list/coordinate-list.component";
+import {CoordinateCreateComponent} from "./coordinate-create/coordinate-create.component";
+import {CoordinateSearchComponent} from "./coordinate-search/coordinate-search.component";
+import {CoordinateUpdateComponent} from "./coordinate-update/coordinate-update.component";
+import {CoordinateDeleteComponent} from "./coordinate-delete/coordinate-delete.component";
+import {CoordinateDetailComponent} from "./coordinate-detail/coordinate-detail.component";
 
 
 const routes: Routes = [
-  {path: 'coordinates/list', component: CoordinateListComponent},
+  {path: 'coordinates', component: CoordinateListComponent},
+  {path: 'coordinates/create', component: CoordinateCreateComponent},
+  {path: 'coordinates/:id', component: CoordinateDetailComponent},
+  {path: 'coordinates/:id/edit', component: CoordinateUpdateComponent},
+  {path: 'coordinates/:id/delete', component: CoordinateDeleteComponent},
 ];
-
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
