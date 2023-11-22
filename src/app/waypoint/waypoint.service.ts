1c4e4c2285f622e4555b1536546de790f7be3bc4
ZcR-01 <zihan1048@live.com>
2023-11-22 18:27:57 +0100

    feat: add waypoint service

---
+++ src/app/waypoint/waypoint.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Waypoint } from './waypoint';
+
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+/* import { Observable } from 'rxjs/internal/Observable'; */
+import {
+  HateoasResourceOperation,
+  ResourceCollection,
+} from '@lagoshny/ngx-hateoas-client';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WaypointService extends HateoasResourceOperation<Waypoint> {
+  constructor(private http: HttpClient) {
+    super(Waypoint);
+  }
+
+  public findByTitle(
+    query: string
+  ): Observable<ResourceCollection<Waypoint>> {
+    return this.searchCollection('findByTitle', {
+      params: { text: query },
+    });
+  }
+}
