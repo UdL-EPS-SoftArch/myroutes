2721cad35106b6d91f2f1690c8c8766aaf2182ef
Gerard <gerardmonso@gmail.com>
2023-12-08 17:53:54 +0100

    coordinates edit

--- src/app/coordinate/coordinate-update/coordinate-update.component.ts
+++ src/app/coordinate/coordinate-update/coordinate-update.component.ts
@@ -1,10 +1,41 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Route} from "../../routes/route";
+import {ActivatedRoute, Router} from "@angular/router";
+import {RouteService} from "../../routes/route.service";
+import {Location} from "@angular/common";
+import {switchMap} from "rxjs/operators";
+import {User} from "../../login-basic/user";
+import {Coordinate} from "../coordinate.entity";
+import {CoordinateService} from "../coordinate.service";
 
 @Component({
   selector: 'app-coordinate-update',
   templateUrl: './coordinate-update.component.html',
   styleUrls: ['./coordinate-update.component.scss']
 })
-export class CoordinateUpdateComponent {
+export class CoordinateUpdateComponent implements OnInit{
+  public coordinatee: Coordinate = new Coordinate();
 
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router,
+              private coordinateService: CoordinateService,
+              private location: Location) { }
+
+  ngOnInit(): void {
+
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.coordinateService.getResource(id).subscribe(
+      (c: Coordinate) => {this.coordinatee = c; this.coordinatee.id = id;});
+  }
+
+  onSubmit() {
+    this.coordinateService.patchResource(this.coordinatee).subscribe(
+      (coordinate: Coordinate) => {
+        this.router.navigate([coordinate.uri]);
+      });
+  }
+
+  goBackToPrevPage(): void {
+    this.location.back();
+  }
 }
