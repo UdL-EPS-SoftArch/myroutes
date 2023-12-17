e42bfbf952732a7e120bee5701b79084f204cc27
Andreu Pérez <andreu@thesolution.es>
2023-12-18 00:58:45 +0100

    Create RouteVersions Create Component

---
+++ src/app/route-versions/route-versions-create/route-versions-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-route-versions-create',
+  templateUrl: './route-versions-create.component.html',
+  styleUrls: ['./route-versions-create.component.scss']
+})
+export class RouteVersionsCreateComponent {
+
+}
