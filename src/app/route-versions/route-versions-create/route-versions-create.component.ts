ac17242c162ffd34128ada716bb7612e52d61a82
Andreu Pérez <andreu@thesolution.es>
2023-12-18 01:03:30 +0100

    Implement OnInit in RouteVersionsCreateComponent

--- src/app/route-versions/route-versions-create/route-versions-create.component.ts
+++ src/app/route-versions/route-versions-create/route-versions-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouteVersionsService} from "../route-versions.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
@@ -8,11 +8,17 @@ import {HttpClient} from "@angular/common/http";
   templateUrl: './route-versions-create.component.html',
   styleUrls: ['./route-versions-create.component.scss']
 })
-export class RouteVersionsCreateComponent {
+export class RouteVersionsCreateComponent implements OnInit{
+  public paramRoute: string;
+
   constructor(
     private routeVersionService: RouteVersionsService,
     private router: Router,
     private route: ActivatedRoute
   ) {
+  }
+
+  ngOnInit(): void {
+    this.paramRoute = this.route.snapshot.params['param']
   }
 }
