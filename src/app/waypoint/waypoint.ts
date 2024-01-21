de688721ce1764a1ec5754c7a61a30391f3856b1
Zihan Chen <zihan1048@live.com>
2024-01-21 13:26:47 +0100

    refactor: comment waypoint id field

--- src/app/waypoint/waypoint.ts
+++ src/app/waypoint/waypoint.ts
@@ -4,6 +4,7 @@ import {Coordinate} from "../coordinate/coordinate.entity";
 @HateoasResource('waypoints')
 export class Waypoint extends Resource {
   uri: string;
+  id: string;
   title: string;
   description: string;
   type: string;
@@ -15,8 +16,8 @@ export class Waypoint extends Resource {
     Object.assign(this as any, values);
   }
 
-  public get id(): string {
-    let uriArray = this.uri.split('/');
-    return uriArray.pop();
-  }
+  // public get id(): string {
+  //   let uriArray = this.uri.split('/');
+  //   return uriArray.pop();
+  // }
 }
